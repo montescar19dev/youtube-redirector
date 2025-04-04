c6987350fae06acc143ac0e3817ccaba52abdad5
Carlos Montes <carlosmontes19.dev@gmail.com>
2025-04-04 07:12:42 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  const youtubeLink = 'https://www.youtube.com/shorts/BMvTjYL3Y_s'; // Cambia esto si quieres
+  res.redirect(302, youtubeLink);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
